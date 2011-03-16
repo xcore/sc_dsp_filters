17465117176998373e676ddddd2abe40335149b4
Henk Muller <henk@xmos.com>
2011-03-16 16:09:16 +0000

    Added missing headers

--- app_example_biquad/src/coeffs.h
+++ app_example_biquad/src/coeffs.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2011, XMOS Ltd, All rights reserved
+// This software is freely distributable under a derivative of the
+// University of Illinois/NCSA Open Source License posted in
+// LICENSE.txt and at <http://github.xcore.com/>
+
 //Generated code - do not edit.
 
 #define BANKS 2
