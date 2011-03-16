3c85ce5debc2aef6c12f05f835f8d9ac7ea5239a
Henk Muller <henk@xmos.com>
2011-03-16 15:46:32 +0000

    Now with bp, lp, hp, and a check on the number of taps

--- build_fir_coefficients/main.c
+++ build_fir_coefficients/main.c
@@ -40,40 +40,87 @@ double sinc(double x, double fc) {
 }
 
 double lp(double fc, window w, int n, int N) {
-    double wi = windowValue(wi, n, N);
+    double wi = windowValue(w, n, N);
     double s = sinc(n-(N>>1), fc);
+    if (N * fc < 4) {
+        printf("Expected taps to be at least %d\n", ((int) (4/fc))|1);
+    }
     return s*wi;
 }
 
-double hp(double fc, window w, int n, int N) {
-    double l = lp(fc, w, n, N);
-    if (n & 1) {
-        return -l;
-    } else {
-        return l;
-    }
-}
-// cool trick from http://www.dsprelated.com/groups/audiodsp/show/1036.php
+// even cooler trick from http://mpastell.com/2010/01/18/fir-with-scipy/
 // to convert low pass to high pass.
 
+double hp(double fc, window w, int n, int N) {
+    double l = lp(fc, w, n, N);
+    if (n != (N>>1)) {
+        return -l;
+    } else {
+        return 1-l;
+    }
+}
 
+/* Band stop, from same website */
 
+double bs(double fcl, double fch, window w, int n, int N) {
+    double l = lp(fcl, w, n, N);
+    double h = hp(fch, w, n, N);
+    return l+h;
+}
+
+/* Band pass, from same website */
+
+double bp(double fcl, double fch, window w, int n, int N) {
+    double b = bs(fcl, fch, w, n, N);
+    if (n != (N>>1)) {
+        return -b;
+    } else {
+        return 1-b;
+    }
+}
+
+// freq response, from http://www.dspguru.com/dsp/faqs/fir/properties
+
+void freq(int N, double h[], double f) {
+    double omega;
+    int i;
+    double factor = sqrt(sqrt(sqrt(sqrt(sqrt(2.0)))));
+    for(omega = 50; omega < f/2; omega = omega * factor) {
+        double sumr = 0;
+        double sumi = 0;
+        double mag;
+
+        for(i = 0; i < N; i++) {
+            double o = omega/f * 2 * pi;
+            sumr += h[i] * cos(i*o);
+            sumi += h[i] * sin(i*o);
+        }
+        mag = sqrt(sumi*sumi + sumr*sumr);
+        printf("%8.0f    %f   %8.4f\n", omega, mag, 20*log10(mag));
+    }
+}
 
 int main(void) {
     double samplingFrequency = 48000;
-    double cutOff = 4800;
-    double fc = cutOff / samplingFrequency;
+    double cutOffl =  800;
+    double fcl = cutOffl / samplingFrequency;
+    double cutOffh = 6400;
+    double fch = cutOffh / samplingFrequency;
     int i;
-    int N = 21;
-    double c[21];
-    double sum = 0.0;
+    int N = 231;
+    double c[231];
 
     for( i = 0; i < N; i++) {
-        c[i] = hp(fc, HAMMING, i, N);
+        c[i] = bp(fcl, fch, HAMMING, i, N);
+//        c[i] = hp(fch, HAMMING, i, N);
     }
 
+    freq(N, c, samplingFrequency);
+/*
     for(i = 0; i < N; i++) {
         sum += c[i];
         printf("%f\n", sum);
     }
+*/
+    return 0;
 }
